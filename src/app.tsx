8127845c1ce6a14ce514bdad59d767d5d61da8e9
Leo Kuo <p900372012@gmail.com>
2024-02-07 22:01:51 +0800

    chore modify nits from basic template

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,5 @@
 import { type Component } from "solid-js";
-import { counter, setCounter } from "./states/counter";
 
 export const App: Component = () => {
-  return (
-    <div>
-      <span>{counter()}</span>
-      <button onclick={() => setCounter((prev) => prev + 1)}>Click</button>
-    </div>
-  );
+  return <></>;
 };
